8ce6c999bb679be149b886bb5e18708e20edca0e
Thiago Holanda <thiago.holanda@concretesolutions.com.br>
2015-11-19 16:13:47 -0200

    ajustes na demonstração

---
+++ IBDesignables/ElementExampleView.h
@@ -0,0 +1,13 @@
+//
+//  ElementExampleView.h
+//  IBDesignables
+//
+//  Created by Thiago Holanda on 11/17/15.
+//  Copyright © 2015 Cocoaheads. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ElementExampleView : UIView
+
+@end
---
+++ IBDesignables/ElementExampleView.m
@@ -0,0 +1,21 @@
+//
+//  ElementExampleView.m
+//  IBDesignables
+//
+//  Created by Thiago Holanda on 11/17/15.
+//  Copyright © 2015 Cocoaheads. All rights reserved.
+//
+
+#import "ElementExampleView.h"
+
+@implementation ElementExampleView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
